c6d3593e7a516e14af6c266d3e4dce21d46819c3
mhxx307 <minhquan.lavo@gmail.com>
2022-12-07 01:28:38 +0700

    create shortcut styles fro tailwind and create a frame for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styles from "./styles";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <div className="bg-black w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>Navbar</div>
+            </div>
+
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>hero</div>
+            </div>
+
+            <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    Stats Business Billing CardDeal Testimonials Clients CTA Footer
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
