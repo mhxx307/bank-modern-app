b78ecd7039064b965871f256afe7647816d9fd6f
mhxx307 <minhquan.lavo@gmail.com>
2022-12-07 15:34:37 +0700

    completed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,9 @@ function Hero() {
             </div>
 
             {/* robot background in right side */}
-            <div>
+            <div
+                className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+            >
                 <img
                     src={robot}
                     alt="robot"
@@ -59,6 +61,10 @@ function Hero() {
                 <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
                 <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
                 <div className="absolute z-[0] w-[50%] h-[55%] right-20 bottom-20 blue__gradient" />
+            </div>
+
+            <div className={`ss:hidden ${styles.flexCenter} `}>
+                <GetStarted />
             </div>
         </section>
     );
